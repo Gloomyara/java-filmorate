bee5b6bf3c4efcccac25a55e38066df5824fb36e
Gloomyara <gloomyara@gmail.com>
2023-03-16 15:27:54 +0400

    Final 9 version 1.2 Feature: @Validation

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -14,12 +14,15 @@ import java.time.LocalDate;
 public class User {
 
     private Integer id;
-    @NotNull(message = "Email cannot be null")
+    @NotNull(message = "Email cannot be null")//иначе создается пользователь с email=null
     @Email(message = "Email should be valid")
     private String email;
     @NotBlank(message = "Login cannot be blank")
     private String login;
     private String name;
+    //Без @NotNull создается пользователь с birthday=null.
+    //В фильме дата NullPointerException выдает т.к. ее проверяю через иф.
+    //Собственную аннотацию пытался сделать, но что-то пошло не так и в итоге она браковала все.
     @Past(message = "Birthday should be in the past")
     private LocalDate birthday;
 }
