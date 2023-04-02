b3af7b83c53066884b26dec3293ce951d5a2508c
Gloomyara <gloomyara@gmail.com>
2023-04-02 22:34:34 +0400

    Staged 11 version 1.0 Style: add description

--- src/main/java/ru/yandex/practicum/filmorate/model/enums/FilmRating.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/enums/FilmRating.java
@@ -1,19 +1,26 @@
 package ru.yandex.practicum.filmorate.model.enums;
 
 public enum FilmRating {
-    G("G"),
-    PG("PG"),
-    PG_13("PG-13"),
-    R("R"),
-    NC_17("NC-17");
+    G("G", "У фильма нет возрастных ограничений"),
+    PG("PG", "Детям рекомендуется смотреть фильм с родителями"),
+    PG_13("PG-13", "Детям до 13 лет просмотр не желателен"),
+    R("R", "Лицам до 17 лет просматривать фильм " +
+            "можно только в присутствии взрослого"),
+    NC_17("NC-17", " лицам до 18 лет просмотр запрещён");
     private final String ratingName;
+    private final String ratingDescription;
 
-    FilmRating(String ratingName) {
+    FilmRating(String ratingName, String ratingDescription) {
         this.ratingName = ratingName;
+        this.ratingDescription = ratingDescription;
     }
 
     public String getRatingName() {
         return ratingName;
+    }
+
+    public String getRatingDescription() {
+        return ratingDescription;
     }
 
     public static FilmRating fromString(String str) {
